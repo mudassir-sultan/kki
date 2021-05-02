d6298bd7084464717304d7a0d1382a8f1cfd448d
mudassar sultan <mudassar@renttango.com>
2021-05-02 11:53:58 +0500

    Product Catagory Page UI updated.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -154,67 +154,29 @@ jQuery(function($) {
     });
 
     // Accordion JS
-    $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
-    $('.accordion a').on("click", function(j) {
-        var dropDown = $(this).closest('li').find('p');
-        $(this).closest('.accordion').find('p').not(dropDown).slideUp();
-        if ($(this).hasClass('active')) {
-            $(this).removeClass('active');
-        } else {
-            $(this).closest('.accordion').find('a.active').removeClass('active');
-            $(this).addClass('active');
-        }
-        dropDown.stop(false, true).slideToggle();
-        j.preventDefault();
-    });
-
-    // -------------
-    // (function($) {
-    //     $.fn.liHarmonica = function(params) {
-    //         var p = $.extend({
-    //             currentClass: 'cur', //Класс для выделенного пункта меню
-    //             onlyOne: true, //true - открытым может быть только один пункт, 
-    //             //false - число открытых одновременно пунктов не ограничено
-    //             speed: 500 //Скорость анимации
-    //         }, params);
-    //         return this.each(function() {
-    //             var
-    //                 el = $(this).addClass('harmonica'),
-    //                 linkItem = $('ul', el).prev('a');
-    //             el.children(':last').addClass('last');
-    //             $('ul', el).each(function() {
-    //                 $(this).children(':last').addClass('last');
-    //             });
-    //             $('ul', el).prev('a').addClass('harFull');
-    //             el.find('.' + p.currentClass).parents('ul').show().prev('a').addClass(p.currentClass).addClass('harOpen');
-    //             linkItem.on('click', function() {
-    //                 if ($(this).next('ul').is(':hidden')) {
-    //                     $(this).addClass('harOpen');
-    //                 } else {
-    //                     $(this).removeClass('harOpen');
-    //                 }
-    //                 if (p.onlyOne) {
-    //                     $(this).closest('ul').closest('ul').find('ul').not($(this).next('ul')).slideUp(p.speed).prev('a').removeClass('harOpen');
-    //                     $(this).next('ul').slideToggle(p.speed);
-    //                 } else {
-    //                     $(this).next('ul').stop(true).slideToggle(p.speed);
-    //                 }
-    //                 return false;
-    //             });
-    //         });
-    //     };
-    // })(jQuery);
-
-    // $(function() {
-    //     $('.anyClass').liHarmonica({
-    //         onlyOne: false,
-    //         speed: 500
-    //     });
-    //     $('.anyClass2').liHarmonica({
-    //         onlyOne: true,
-    //         speed: 400
-    //     });
+    // $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+    // $('.accordion a').on("click", function(j) {
+    //     var dropDown = $(this).closest('li').find('p');
+    //     $(this).closest('.accordion').find('p').not(dropDown).slideUp();
+    //     if ($(this).hasClass('active')) {
+    //         $(this).removeClass('active');
+    //     } else {
+    //         $(this).closest('.accordion').find('a.active').removeClass('active');
+    //         $(this).addClass('active');
+    //     }
+    //     dropDown.stop(false, true).slideToggle();
+    //     j.preventDefault();
     // });
+    var toggler = document.getElementsByClassName("parrent-item");
+    var i;
+
+    for (i = 0; i < toggler.length; i++) {
+        toggler[i].addEventListener("click", function() {
+            this.parentElement.querySelector(".child-item").classList.toggle("isActive");
+            this.classList.toggle("check-box");
+        });
+    }
+
     // -------------
     // Timer JS
     let getDaysId = document.getElementById('days');
